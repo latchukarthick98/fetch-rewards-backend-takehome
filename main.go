fc00a412a4b6f6ba9454e8a746b4f17ca3e3d366
Lakshman Karthik Ramkumar <lathuckarthick98@gmail.com>
2022-11-03 20:55:30 -0500

    Setup Gin and ENV

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func getEnvVar(key string) string {
+
+	// Load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+
+	// Initialize Gin
+	r := gin.Default()
+
+	port := getEnvVar("PORT")
+	fmt.Printf("Port %s \n", port)
+	// Initialze Server on Port 3001
+	r.Run(":" + port)
+}
